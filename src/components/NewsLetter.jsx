495afcccd1b434de61dfea8422ab781dc9b53dbe
MursalinDevOps <mursalin1768@gmail.com>
2025-02-11 12:59:40 +0600

    sweet alert added on newsletter section

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,13 +1,24 @@
+import Swal from "sweetalert2";
+
 const NewsLetter = () => {
+    const handleImIn = (e) =>{
+        e.preventDefault();
+        Swal.fire({
+            title: "Good job!",
+            text: "You're In!",
+            icon: "success"
+          });
+          form.reset();
+    }
     return (
         <div className="h-[600px] flex justify-center items-center">
             <div className="border border-blue-300 rounded-lg py-24 flex items-center flex-col md:w-1/2 space-x-5 space-y-3">
                 <h2 className="text-2xl md:text-4xl font-semibold text-blue-500">Join The Community</h2>
                 <p className=" text-center">Hi there! Subscribe to our newspaper to stay updated.</p>
-                <div className="join">
-                    <input className="input input-bordered join-item md:pr-20" placeholder="Email" />
-                    <button className="btn join-item md:px-10 bg-blue-500 text-white">I'M IN</button>
-                </div>
+                <form onSubmit={handleImIn} className="join max-w-fit flex justify-center">
+                    <input className="input input-bordered join-item md:pr-20" required placeholder="Email" />
+                    <button className="btn join-item md:px-10 bg-blue-500 mr-5 text-white">I'M IN</button>
+                </form>
                 <p>We don't spam! pinky promise :)</p>
             </div>
         </div>
